b82c67ff059e69d99351e5cb5602f55dae221477
Alex Chaplygin <chapalexander@gmail.com>
2023-03-01 21:23:05 +0300

    Correct Animation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,10 @@ public class PlayerMovement : MonoBehaviour
     private float speed;
     private Vector3 velocity;
     private Rigidbody2D rb;
+    private bool IsNearWall = false;
+    private float threshold = 0.1f; //for IsMoving() Method
+    private Vector2 previousPosition;
+    private bool isMoving = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,25 +22,40 @@ public class PlayerMovement : MonoBehaviour
         speed = data.speed;
     }
 
-    //private void Update()
-    //{
-    //    transform.position += Time.deltaTime * velocity;
-    //}
+   
 
     private void FixedUpdate()
     {
         rb.velocity = velocity;
+        if (!IsMoving()) animator.SetFloat("Speed", 0);
+        else animator.SetFloat("Speed", velocity.magnitude);
+    }
+
+    private void Update()
+    {
+       
     }
 
     public void OnMove(InputValue inputValue)
     {
-       velocity = speed * inputValue.Get<Vector2>();
-       Debug.Log(velocity);
-       animator.SetFloat("Speed", velocity.magnitude);
-       animator.SetFloat("HorizontalSpeed", velocity.x);
-       animator.SetFloat("VerticalSpeed", velocity.y);
-       
+        Vector2 inputVector = inputValue.Get<Vector2>();
+        if (inputVector.magnitude == 0) return;
+        velocity = speed * inputVector;
+        animator.SetFloat("HorizontalSpeed", velocity.x);
+        animator.SetFloat("VerticalSpeed", velocity.y);
     }
 
-   
+    private bool IsMoving()
+    {
+        Vector2 currentPosition = transform.position;
+        Debug.Log(currentPosition + " "+ previousPosition);
+        if ((previousPosition - currentPosition).magnitude > 0) isMoving = true;
+        else isMoving = false;
+        Debug.Log((previousPosition - currentPosition).magnitude);
+        previousPosition = currentPosition;
+        return isMoving;
+    }
+
+
+
 }

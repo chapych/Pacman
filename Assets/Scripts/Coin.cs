0eac25c18d44e805744a2f771be4cf46209a74e0
Alex Chaplygin <chapalexander@gmail.com>
2023-03-07 22:05:51 +0300

    Correct Collecting Dots System

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -12,7 +12,6 @@ public class Coin : MonoBehaviour, ICollectable
     }
     public void ObtainItemHandler()
     {
-        Debug.Log("smth");
         Destroy(this);
     }
 
--- Assets/Scripts/InteractionWithElements.cs
+++ Assets/Scripts/InteractionWithElements.cs
@@ -8,23 +8,17 @@ public class InteractionWithElements : MonoBehaviour
 {
     [SerializeField]
     private Tilemap tilemap;
-    [SerializeField]
-    private LayerMask dotsLayer;
-    
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        ICollectable collectableItem = collision.collider.gameObject.GetComponent<ICollectable>();
-        Debug.Log(collision.collider.gameObject.layer);
-        if (collision.collider.gameObject.layer == dotsLayer)
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Dots"))
         {
             Vector2 hitPosition = Vector2.zero;
+
             foreach (ContactPoint2D hit in collision.contacts)
             {
                 hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
                 hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
                 tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
-                Debug.Log(hitPosition);
             }
         }
     }
